fbd46cd295ca3ff1c268dbb1eab2cb0a20f1aa3c
Anchal Aithani <anchalaithani@gmail.com>
2018-08-07 06:54:21 +0530

    class implementation1

--- Stone_Paper_Scissors.py
+++ Stone_Paper_Scissors.py
@@ -16,19 +16,19 @@ while not player:
         print("Computer = ", computer, "\nTie!")
     elif player == "Stone":
         if computer == "Paper":
-            print("Computer = ", computer, "\nYou lose!", computer, "covers", player)
+            print("Computer = ", computer, "\nYou lose..!!!", computer, "covers", player)
         else:
-            print("Computer = ", computer, "\nYou win!", player, "smashes", computer)
+            print("Computer = ", computer, "\nYou win...!!!", player, "smashes", computer)
     elif player == "Paper":
         if computer == "Scissors":
-            print("Computer = ", computer, "\nYou lose!", computer, "cut", player)
+            print("Computer = ", computer, "\nYou lose...!!!!", computer, "cut", player)
         else:
-            print("Computer = ", computer, "\nYou win!", player, "covers", computer)
+            print("Computer = ", computer, "\nYou win..!!!", player, "covers", computer)
     elif player == "Scissors":
         if computer == "Stone":
-            print("Computer = ", computer, "\nYou lose...", computer, "smashes", player)
+            print("Computer = ", computer, "\nYou lose...!!", computer, "smashes", player)
         else:
-            print("Computer = ", computer, "\nYou win!", player, "cut", computer)
+            print("Computer = ", computer, "\nYou win..!!!", player, "cut", computer)
     else:
         print("That's not a valid play. Check your spelling!")
     # player was set to True, but we want it to be False so the loop continues
