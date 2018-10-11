34189651d6120b0b4a5ef82645cef68cd4700d96
ANCHAL AITHANI <38647876+Anchal2096@users.noreply.github.com>
2018-10-11 08:48:37 +0530

    a rough sketch

---
+++ Getting_familiar_with_python_gui/trial_login_page.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+gui = Tk()
+gui.geometry("400x400")
+#make sure first is capital and second is not
+gui.title("First title")
+a = Label(gui ,text="username").grid(row=0,column = 0)
+b = Label(gui ,text="password").grid(row=1,column=0)
+e = Entry(gui).grid(row=0,column=1)
+f = Entry(gui,show="**").grid(row=1,column=1)
+c = Button(gui,text="LOGIN").grid(row=2,column=0)
+gui.mainloop()
