c9d0f2fc601cee33f76b36d6d4adc90e54b3d425
ANCHAL AITHANI <38647876+Anchal2096@users.noreply.github.com>
2018-10-12 21:34:00 +0530

    get() working but look has to be improved

--- Getting_familiar_with_python_gui/trial_login_page.py
+++ Getting_familiar_with_python_gui/trial_login_page.py
@@ -1,32 +1,43 @@
 from tkinter import *
 import tkinter.messagebox as tm
 
-gui = Tk()
-# Main window properties
-gui.geometry("360x125")
-gui.title("First title")
-gui.resizable(False, False)
 
-# widgets in the main Window
-username = Label(gui, text="username").grid(row=0, column=0)
-password = Label(gui, text="password").grid(row=1, column=0)
-name_field = Entry(gui).grid(row=0, column=1)
-password_field = Entry(gui, show="**").grid(row=1, column=1)
-checkbox = Checkbutton(gui, text="Keep me logged in").grid(columnspan=2)
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_username = Label(self, text="Username")
+        self.label_password = Label(self, text="Password")
+
+        self.entry_username = Entry(self)
+        self.entry_password = Entry(self, show="*")
+
+        self.label_username.grid(row=0, column=0)
+        self.label_password.grid(row=1, column=0)
+        self.entry_username.grid(row=0, column=1)
+        self.entry_password.grid(row=1, column=1)
+
+        self.checkbox = Checkbutton(self, text="Keep me logged in")
+        self.checkbox.grid(columnspan=2)
+
+        self.logbtn = Button(self, text="Login", command=self._login_btn_clicked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clicked(self):
+        # print("Clicked")
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        # print(username, password)
+
+        if username == "admin" and password == "dcs@123":
+            tm.showinfo("Login info", "Welcome Admin")
+        else:
+            tm.showerror("Login error", "Incorrect username")
 
 
-def _login_btn_clicked():
-    # print("Clicked")
-   # username_var = name_field.get()
-   # password_var = password_field.get()
-
-    # print(username, password)
-
-    if name_field.get == "admin" and password_field.get == "dcs@123":
-        tm.showinfo("Login info", "Welcome Admin")
-    else:
-        tm.showerror("Login error", "Incorrect username")
-
-
-command_button = Button(gui, text="login", command=_login_btn_clicked).grid(row=3, column=1)
-gui.mainloop()
+root = Tk()
+lf = LoginFrame(root)
+root.mainloop()
