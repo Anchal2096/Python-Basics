03a8db9469b8d4f7c3af50ee4390e6138a8dd3f9
ANCHAL AITHANI <38647876+Anchal2096@users.noreply.github.com>
2018-10-12 08:06:39 +0530

    get()not working

--- Getting_familiar_with_python_gui/trial_login_page.py
+++ Getting_familiar_with_python_gui/trial_login_page.py
@@ -1,8 +1,9 @@
 from tkinter import *
+import tkinter.messagebox as tm
 
 gui = Tk()
 # Main window properties
-gui.geometry("400x400")
+gui.geometry("360x125")
 gui.title("First title")
 gui.resizable(False, False)
 
@@ -11,6 +12,21 @@ username = Label(gui, text="username").grid(row=0, column=0)
 password = Label(gui, text="password").grid(row=1, column=0)
 name_field = Entry(gui).grid(row=0, column=1)
 password_field = Entry(gui, show="**").grid(row=1, column=1)
-submit_button = Button(gui, text="LOGIN").grid(row=2, column=0)
+checkbox = Checkbutton(gui, text="Keep me logged in").grid(columnspan=2)
 
+
+def _login_btn_clicked():
+    # print("Clicked")
+   # username_var = name_field.get()
+   # password_var = password_field.get()
+
+    # print(username, password)
+
+    if name_field.get == "admin" and password_field.get == "dcs@123":
+        tm.showinfo("Login info", "Welcome Admin")
+    else:
+        tm.showerror("Login error", "Incorrect username")
+
+
+command_button = Button(gui, text="login", command=_login_btn_clicked).grid(row=3, column=1)
 gui.mainloop()
